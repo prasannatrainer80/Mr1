38837324d081494004d3438c367fb2a3cd1ead01
prasannatrainer80 <prasanna.trainer.java@gmail.com>
2026-07-14 11:41:16 +0530

    code

---
+++ login-src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+import First from './components/first/first';
+import Second from './components/second/second';
+import Third from './components/third/third';
+import Four from './components/four/four';
+import Five from './components/five/five';
+import Six from './components/six/six';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Menu from './components/menu/menu';
+import Login from './components/login/login';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/first" element={<First />} />
+          <Route path="/second" element={<Second />} />
+          <Route path="/third" element={<Third />} />
+          <Route path="/four" element={<Four />} />
+          <Route path="/five" element={<Five />} />
+          <Route path="six" element={<Six />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
